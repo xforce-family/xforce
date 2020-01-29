5c8a70119a7da95a3413b8d705441156868aa064
Holfz <holfzcompilation@gmail.com>
2020-01-29 19:59:34 +0700

    nongview48: Finishing up mute command

--- bot_nongview48/commands/mute.js
+++ bot_nongview48/commands/mute.js
@@ -1,5 +1,8 @@
 /* Helpers */
+const ms = require('ms');
 const _ = require('lodash');
+const async = require('async');
+const moment = require('moment');
 const { logInfo, logOk, logWarn, logError } = require('../helpers/logger');
 
 /* Function */
@@ -16,6 +19,109 @@ function Random_Message(db_table, cb = null) {
     }
 }
 
+function Process(channels, mentions, args, cb = null) {
+    // We created table for contain processed data
+    let Data = {
+        'Channels': [],
+        'Users': []
+    };
+
+    // We're gonna process the channel data first
+    async.each(channels, function(channel, callback) {
+        let channel_id = channel.id;
+        
+        //Push the id into list that we will announce about mute
+        Data.Channels.push(channel_id);
+
+        //And Exclude it
+        _.pull(args, "<#" + channel_id + ">");
+
+        // Next
+        callback();
+    }, function(err) {
+        // Next, We gonna process the client data
+        async.each(mentions, function(mention, callback) {
+            let member_id = mention.id;
+            let member_data = global.xforce_guild.members.get(member_id);
+            let Moderator = !!member_data.roles.find(role => role.name === 'Moderator')
+
+            //Push the info into list that we will mute
+            Data.Users.push({id: member_id, isModerator: Moderator});
+
+            //And Exclude it
+            _.pull(args, "<@!" + member_id + ">");
+
+            // Next
+            callback();
+        }, function(err) {
+            if(cb != null) {
+                return cb(Data);
+            }
+        });
+    });
+}
+
+function Performing_Mute(data, client, message) {
+    let Users = data.Users;
+    let Channels = data.Channels;
+    let Message = data.Message;
+    let Converted_Duration = ms(data.Duration);
+
+    let Summary_Array = {
+        Normal: [],
+        Moderator: []
+    };
+
+    async.each(Users, function(user, callback) {
+        let db_get = global.lowsession.get('muted').find({ id: user.id }).value();
+
+        if(!db_get) {
+            let user_data = global.xforce_guild.members.get(user.id);
+
+            if(user.isModerator) {
+                user_data.addRole(global.moderator_mute_role);
+
+                Summary_Array.Moderator.push("<@" + user.id + ">");
+            } else {
+                user_data.addRole(global.default_mute_role);
+
+                Summary_Array.Normal.push("<@" + user.id + ">");
+            }
+
+            user.expired = moment().add(Converted_Duration, 'milliseconds');
+            user.channels = Channels;
+            global.lowsession.get('muted').push(user).write();
+        }
+
+        callback();
+    }, function(err) {
+        if (Summary_Array.Normal.length <= 0 && Summary_Array.Moderator.length <= 0) { return };
+
+        let announce_message = "";
+        if (Summary_Array.Normal.length > 0) {
+            announce_message += "Users " + Summary_Array.Normal.join(', ') + " ถูก " + "<@&" + global.default_mute_role + ">";
+        }
+
+        if (Summary_Array.Moderator.length > 0) {
+            if(announce_message !== "") {
+                announce_message += "และ";
+            }
+
+            announce_message += "Users " + Summary_Array.Moderator.join(', ') + " ถูก " + "<@&" + global.moderator_mute_role + ">"
+        }
+
+        announce_message += " เป็นเวลา " + ms(Converted_Duration, { long: true });
+
+        async.each(Channels, function(channel, callback) {
+            client.channels.get(channel).send(announce_message).then(msg => {
+                msg.channel.send(Message);
+            })
+
+            callback();
+        });
+    });
+}
+
 module.exports = {
     name: 'mute',
     description: 'ใบ้จ้า',
@@ -28,6 +134,44 @@ module.exports = {
             return;
         }
 
+        /* Map some data that not need to process */
+        let duration = args[1];
+
+        /* Exclude some data that not need to process */
+        args.shift(); // <- This is "mute" command
+        _.pull(args, duration); // <- This is "duration" data
+
+        /* Process Data */
         
+        // Map all data that need to process
+        let channels = message.mentions.channels.array();
+        let mentions = message.mentions.users.array();
+
+        //If channel is not mentioned. We assume current channel as a channel.
+        channels = [];
+        channels.push(message.channel);
+
+        // Then process the data
+        Process(channels, mentions, args, (Data) => {
+            //Set more data
+            Data.Duration = duration;
+
+            //Free some memory
+            delete mentions;
+            delete duration;
+
+            //Final one is args. We'll see if args is empty, Means we'll random some mute message. If not, we'll take it as custom message.
+            if(args.length <= 0) {
+                Random_Message("mute_message", (randomed_msg) => {
+                    if(randomed_msg) { Data.Message = randomed_msg };
+
+                    Performing_Mute(Data, client, message);
+                });    
+            } else {
+                Data.Message = args.join(' ');
+
+                Performing_Mute(Data, client, message);
+            }
+        });
     }
 };
--- bot_nongview48/nong_view_48.js
+++ bot_nongview48/nong_view_48.js
@@ -1,6 +1,8 @@
 /* Dependencies */
 const fs = require('fs');
 const low = require('lowdb');
+const async = require('async');
+const moment = require('moment');
 const Discord = require('discord.js');
 
 /* Client */
@@ -10,7 +12,7 @@ const client = new Discord.Client();
 const { logInfo, logOk, logWarn, logError } = require('./helpers/logger');
 
 /* Variable */
-const { prefix, token } = require("./config.json");
+const { prefix, token, xforce_guild_id } = require("./config.json");
 
 /* Commands Loader */
 client.commands = new Discord.Collection();
@@ -30,15 +32,69 @@ const adapter = new FileSync('db.json')
 const db = low(adapter);
 db.defaults({ 
     nopermission_message: [], 
-    mute_message: [] 
+    mute_message: [],
+    muted: []
 }).write();
 
 /* Globals */
 global.lowsession = db;
+global.xforce_guild = null;
+global.default_mute_role = "433462069702950923"
+global.moderator_mute_role = "478921501442179092"
+
+/* Function */
 
 /* Event */
 client.on('ready', () => {
     logOk(`Logged in as ${client.user.tag}!`, 'Discord');
+    global.xforce_guild = client.guilds.get(xforce_guild_id);
+
+    client.setInterval(() => {
+        let Muted_Data = db.get('muted').value()
+        let now = moment();
+
+        async.each(Muted_Data, function(data, callback) {
+            let expired = moment(data.expired);
+            let diff = now.diff(moment(expired));
+
+            if(diff < 0) {
+                return callback();
+            }
+
+            let user_id = data.id;
+            let user_isModerator = data.isModerator;
+            let user_data = global.xforce_guild.members.get(user_id);
+            
+            if(user_isModerator) {
+                let Muted_Role = !!user_data.roles.find(role => role.name === 'ใบ้หัวแดง')
+                
+                if(Muted_Role) {
+                    user_data.removeRole(global.moderator_mute_role);
+
+                    async.each(data.channels, function(channel, cb) {
+                        client.channels.get(channel).send("User <@" + user_id + "> หลุดพ้นจากการ " + "<@&" + global.moderator_mute_role + ">");
+
+                        cb();
+                    });
+                }
+            } else {
+                let Muted_Role = !!user_data.roles.find(role => role.name === 'ใบ้แดก')
+
+                if(Muted_Role) {
+                    user_data.removeRole(global.default_mute_role);
+
+                    async.each(data.channels, function(channel, cb) {
+                        client.channels.get(channel).send("User <@" + user_id + "> หลุดพ้นจากการ " + "<@&" + global.default_mute_role + ">");
+
+                        cb();
+                    });
+                }
+            }
+
+            db.get('muted').remove({ id: user_id }).write()
+            callback();
+        });
+    }, 1000)
 });
 
 client.on('message', message => {
@@ -53,7 +109,7 @@ client.on('message', message => {
     if (!client.commands.has(command)) { return; }
 
     try {
-        client.commands.get(command).execute(client, message, args, db);
+        client.commands.get(command).execute(client, message, args);
     } catch (error) {
         logError(error, 'CommandHandler');
         message.reply('เออเร่อ... ติดต่อน้องโม่ยให้หน่อยยยย');
