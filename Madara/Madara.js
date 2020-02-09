027129dd2acabcc00e663f6aee46f6a89ade0425
Holfz <holfzcompilation@gmail.com>
2020-02-10 02:17:03 +0700

    Madara: FASTER, Raw Event

--- Madara/Madara.js
+++ Madara/Madara.js
@@ -16,18 +16,20 @@ client.on('ready', () => {
     global.xforce_guild = client.guilds.get(xforce_guild_id);
 });
 
-client.on('messageReactionAdd', (reaction, user) => {
+client.on('MadaraReactionAdd', (reaction, user) => {
+    if(!reaction) { return };
     if(reaction._emoji.id !== "446705009707450368") { return };
 
-    if(reaction.count == Bai) {
-        setImmediate(() => {
+    setImmediate(() => {
+        if(reaction.count == Bai) {
             reaction.message.react("446705009707450368");
-        })
+            
 
-        logOk("Message ID : " + reaction.message.id + " | Baimun Count : " + reaction.count + " (Triggered)", "ReactionAdd")
-    } else {
-        logInfo("Message ID : " + reaction.message.id + " | Baimun Count : " + reaction.count, "ReactionAdd");
-    }
+            logOk("Message ID : " + reaction.message.id + " | Baimun Count : " + reaction.count + " (Triggered)", "ReactionAdd")
+        } else {
+            logInfo("Message ID : " + reaction.message.id + " | Baimun Count : " + reaction.count, "ReactionAdd");
+        }
+    })
 });
 
 /* 
@@ -35,26 +37,23 @@ client.on('messageReactionAdd', (reaction, user) => {
     https://github.com/AnIdiotsGuide/discordjs-bot-guide/blob/master/coding-guides/raw-events.md 
 */
 client.on('raw', packet => {
-    if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
+    if (!['MESSAGE_REACTION_ADD'].includes(packet.t)) return;
     // Grab the channel to check the message from
-    const channel = client.channels.get(packet.d.channel_id);
-    // There's no need to emit if the message is cached, because the event will fire anyway for that
-    if (channel.messages.has(packet.d.message_id)) return;
-    // Since we have confirmed the message is not cached, let's fetch it
-    channel.fetchMessage(packet.d.message_id).then(message => {
-        // Emojis can have identifiers of name:id format, so we have to account for that case as well
-        const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
-        // This gives us the reaction we need to emit the event properly, in top of the message object
-        const reaction = message.reactions.get(emoji);
-        // Adds the currently reacting user to the reaction's users collection.
-        if (reaction) reaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
-        // Check which type of event it is before emitting
-        if (packet.t === 'MESSAGE_REACTION_ADD') {
-            client.emit('messageReactionAdd', reaction, client.users.get(packet.d.user_id));
-        }
-        if (packet.t === 'MESSAGE_REACTION_REMOVE') {
-            client.emit('messageReactionRemove', reaction, client.users.get(packet.d.user_id));
-        }
+    setImmediate(() => {
+        const channel = client.channels.get(packet.d.channel_id);
+        // Since we have confirmed the message is not cached, let's fetch it
+        channel.fetchMessage(packet.d.message_id).then(message => {
+            // Emojis can have identifiers of name:id format, so we have to account for that case as well
+            const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
+            // This gives us the reaction we need to emit the event properly, in top of the message object
+            const reaction = message.reactions.get(emoji);
+            // Adds the currently reacting user to the reaction's users collection.
+            if (reaction) reaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
+            // Check which type of event it is before emitting
+            if (packet.t === 'MESSAGE_REACTION_ADD') {
+                client.emit('MadaraReactionAdd', reaction, client.users.get(packet.d.user_id));
+            }
+        });
     });
 });
 
